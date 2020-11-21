fef2bcfa411402342b1a506c7d4f5ff91d3110dc
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-21 22:07:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,10 +5,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => 
+  monopoly = {:railroads=> 
   {
-    :pieces=> 4 ,
-    :rent_in_dollars=> {
+    :pieces=>4 ,
+    :rent_in_dollars=>{
     }
     :names=> { 
     }   
